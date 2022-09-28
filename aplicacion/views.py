2290818de004950370ef163c695cb8e87a2dd3e7
valeeBurgos <vale.burgos.sanhueza@gmail.com>
2022-09-28 18:52:34 -0300

    Unas pruebas mas uwu

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import gastos
 
 # Create your views here.
 def gastos_list(request):
-    return render(request, 'aplicacion/gastos_list.html', {})
+    gastos_o = gastos.objects.filter(fecha_cobro=timezone.now()).order_by('fecha_cobro')
+
+    return render(request, 'aplicacion/gastos_list.html', {'gastos_o' :gastos_o})
