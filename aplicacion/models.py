3190b9ba5fdd9b8372b6f0d636b1e1b142911129
PaulaCG <paula.cabrera.ff@gmail.com>
2022-10-07 21:18:17 -0300

    arreglado opciones form, falta investigar la fecha

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,17 +1,35 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.admin import widgets 
 
 
 class gastos(models.Model):
     id_usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    recurrente = models.CharField(max_length=2)
+    titulo = models.CharField(max_length=20)
+    descripcion = models.TextField(blank=True, null=True)
+    RECURRENCIA = [
+        ('Si', 'Si'),
+        ('No', 'No'),
+    ]
+    recurrente = models.CharField(max_length=2, choices=RECURRENCIA)
     fecha_cobro = models.DateField()
-    prioridad = models.CharField(max_length=10)
+    PRIORIDAD = [
+        ('Alta', 'Alta'),
+        ('Media', 'Media'),
+        ('Baja', 'Baja'), 
+    ]
+    prioridad = models.CharField(max_length=10, choices=PRIORIDAD)
     monto = models.IntegerField()
-    categoria = models.CharField(max_length=100)
+    CATEGORIA = [
+        ('Entretención', 'Entretención'),
+        ('Transporte', 'Transporte'),
+        ('Alimento', 'Alimentos'),
+        ('Cuentas Básicas', 'Cuentas Básicas'),
+        ('Dividendo', 'Dividendo'),
+        ('Otros', 'Otros'),
+    ]
+    categoria = models.CharField(max_length=100, choices=CATEGORIA)
 
 
     def __str__(self):
