0f6063d8ad80ea102f119f052731cb8f710f7f8c
Condorchavo <vicente.gatica@ug.uchile.cl>
2022-10-10 15:54:16 -0300

    sp1

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -40,18 +40,33 @@ def gastos_new(request):
 def resumen(request):
     if request.method == 'GET':
         gastos_o = gastos.objects.all()
-        e = request.GET.get('data-range')
+        # e = request.GET.get('data-range') Esto debería obtener la opción escogida en el select
         final_date = timezone.now()
-        init_date = timezone.now().date() - timedelta(days=30) #Default
-        if (e == "1"):
-            init_date = timezone.now().date() - timedelta(days=7)
-        elif (e == "2"):
-            init_date = timezone.now().date() - timedelta(days=30)
-        elif (e == "3"):
-            init_date = timezone.now().date() - timedelta(days=60)
-        gastos_o = gastos.objects.filter(fecha_cobro__range=[init_date, final_date])
-        return render(request, 'aplicacion/resumen.html', {'gastos_o' :gastos_o, 'final_date': final_date, 'init_date': e})
-    else:
-        gastos_o = gastos.objects.all()
-        return render(request, 'aplicacion/resumen.html', {'gastos_o' :gastos_o})
+        init_date_1 = timezone.now().date() - timedelta(days=7)
+        init_date_2 = timezone.now().date() - timedelta(days=30)
+        init_date_3 = timezone.now().date() - timedelta(days=60)
+        gastos1 = gastos.objects.filter(fecha_cobro__range=[init_date_1, final_date]) #Tabla opción 1
+        gastos2 = gastos.objects.filter(fecha_cobro__range=[init_date_2, final_date]) #Tabla opción 2
+        gastos3 = gastos.objects.filter(fecha_cobro__range=[init_date_3, final_date]) #Tabla opción 
+        return render(request, 'aplicacion/resumen.html', {'gastos_o': gastos2, # Este es el valor default
+                        'gastos1' :gastos1, 'gastos2': gastos2, 'gastos3': gastos3})
 
+################################################
+# def resumen_w_filter(request): #En teoría esta opción debería funcionar una vez se recupere bien el valor de la variable "e"
+#     if request.method == 'GET':
+#         gastos_o = gastos.objects.all()
+#         e = request.GET.get('data-range')
+#         final_date = timezone.now()
+#         init_date = timezone.now().date() - timedelta(days=30) #Default
+#         if (e == "1"):
+#             init_date = timezone.now().date() - timedelta(days=7)
+#         elif (e == "2"):
+#             init_date = timezone.now().date() - timedelta(days=30)
+#         elif (e == "3"):
+#             init_date = timezone.now().date() - timedelta(days=60)
+#         gastos_o = gastos.objects.filter(fecha_cobro__range=[init_date, final_date])
+#         return render(request, 'aplicacion/resumen.html', {'gastos_o' :gastos_o})
+#     else:
+#         gastos_o = gastos.objects.all()
+#         return render(request, 'aplicacion/resumen.html', {'gastos_o' :gastos_o})
+################################################
