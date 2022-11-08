23b0d01b95be9af7c37f84e89d9043097c5520c4
PaulaCG <paula.cabrera.ff@gmail.com>
2022-11-08 13:08:18 -0300

    arreglado url resumen

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.gastos_list, name='gastos_list'),
     path('gastos/new', views.gastos_new, name='gastos_new'),
     path('gastos/<int:pk>/', views.gastos_detail, name='gastos_detail'),
-    path('gastos/resumen/', views.resumen, name='resumen'),
+    path('/resumen/', views.resumen, name='resumen'),
 
 
 
