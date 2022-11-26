ed5347c1bcd1541dbc09721a90c0859480036310
valeeBurgos <vale.burgos.sanhueza@gmail.com>
2022-11-26 09:53:33 -0300

    copia

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio_calendario, name='inicio'),
+    path('', views.gastos_list, name='gastos_list'),
     path('gastos/new', views.gastos_new, name='gastos_new'),
     path('gastos/<int:pk>/', views.gastos_detail, name='gastos_detail'),
     path('resumen/', views.resumen, name='resumen'),
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -25,7 +25,7 @@ def register(request):
 
     return render(request, 'aplicacion/registro.html', {'form':form})
 
-def inicio_calendario(request):
+def gastos_list(request):
     gastos_o = gastos.objects.all()
     return render(request, 'aplicacion/inicio.html', {'gastos_o' :gastos_o})
 
