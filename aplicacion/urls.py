b1f86a25930e7523ffcd9c0095ffff8c739a7ef1
valeeBurgos <vale.burgos.sanhueza@gmail.com>
2022-11-26 10:55:33 -0300

    index visible

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gastos_list, name='gastos_list'),
+    path('calendario', views.calendario, name='calendario'),
     path('gastos/new', views.gastos_new, name='gastos_new'),
     path('gastos/<int:pk>/', views.gastos_detail, name='gastos_detail'),
     path('resumen/', views.resumen, name='resumen'),
+    path('', views.index, name='index'),
 
 
-    path('', views.ingresos_list, name='ingresos_list'),
     path('ingresos/new', views.ingresos_new, name='ingresos_new'),
     path('ingresos/<int:pk>/', views.ingresos_detail, name='ingresos_detail'),
     path('register/', views.register, name='register'),
